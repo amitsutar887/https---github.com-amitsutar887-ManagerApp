0cfd9cca493a676bca9fd683c03443499ee8b9ab
Amit Sutar <amisutar.87@gmail.com>
2024-07-24 23:24:30 -0700

    changed

--- ManagerApp/src/app/app.module.ts
+++ ManagerApp/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
   declarations: [
     AppComponent
